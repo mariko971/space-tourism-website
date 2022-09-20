3014f8a77a33e57ed2b9cbd16d77edfddc824897
Mark Njuru <66862952+mariko971@users.noreply.github.com>
2022-09-20 14:48:36 +0400

    created backgrounds.js files to facilitate background images for our app component in relation tto screens size. Added Barlow font

---
+++ src/backgrounds.js
@@ -0,0 +1,20 @@
+export const backgrounds = {
+  mobile: {
+    home: "url(/assets/home/background-home-mobile.jpg)",
+    destination: "url(/assets/destination/background-destination-mobile.jpg)",
+    crew: "url(/assets/crew/background-crew-mobile.jpg)",
+    technology: "url(/assets/technology/background-technology-mobile.jpg)",
+  },
+  tablet: {
+    home: "url(/assets/home/background-home-tablet.jpg)",
+    destination: "url(/assets/destination/background-destination-tablet.jpg)",
+    crew: "url(/assets/crew/background-crew-tablet.jpg)",
+    technology: "url(/assets/technology/background-technology-tablet.jpg)",
+  },
+  desktop: {
+    home: "url(/assets/home/background-home-desktop.jpg)",
+    destination: "url(/assets/destination/background-destination-desktop.jpg)",
+    crew: "url(/assets/crew/background-crew-desktop.jpg)",
+    technology: "url(/assets/technology/background-technology-desktop.jpg)",
+  },
+};
--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,36 @@
-import React, { createContext, useEffect } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import data from "../data.json";
 import Home from "../components/home/Home";
 import Destination from "./destination/Destination";
+import Crew from "./crew/crew";
+import Technology from "./technology/technology";
+import { backgrounds } from "../backgrounds";
 import Header from "./header/Header";
 
 import "./App.css";
 
 export const appContext = createContext(data);
+
 const App = () => {
-  let url = window.location.pathname;
-  let backgroundUrl = 'url("/assets/home/background-home-mobile.jpg")';
+  const [imgUrl, setImgUrl] = useState("home");
+  const { mobile, tablet, desktop } = backgrounds;
 
   return (
     <appContext.Provider value={data}>
       <div
         className="app-container"
         style={{
-          backgroundImage:
-            'url("/assets/technology/background-technology-mobile.jpg")',
+          backgroundImage: mobile[imgUrl],
         }}
       >
-        <Header />
+        <Header setImgUrl={setImgUrl} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="destination" element={<Destination />} />
+          <Route path="crew" element={<Crew />} />
+          <Route path="technology" element={<Technology />} />
         </Routes>
       </div>
     </appContext.Provider>
