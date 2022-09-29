26580b05a6e6158088c343209d6bef7e1e08e913
Mark Njuru <66862952+mariko971@users.noreply.github.com>
2022-09-29 13:34:49 +0400

    capitalized folder names in assets folder to remove name conflict

--- src/backgrounds.js
+++ src/backgrounds.js
@@ -1,22 +1,22 @@
 export const pagesBg = {
   home: {
-    desktop: "/assets/home/background-home-desktop.jpg",
-    tablet: "/assets/home/background-home-tablet.jpg",
-    mobile: "/assets/home/background-home-mobile.jpg",
+    desktop: "/assets/Home/background-home-desktop.jpg",
+    tablet: "/assets/Home/background-home-tablet.jpg",
+    mobile: "/assets/Home/background-home-mobile.jpg",
   },
   destination: {
-    desktop: "/assets/destination/background-destination-desktop.jpg",
-    tablet: "/assets/destination/background-destination-tablet.jpg",
-    mobile: "/assets/destination/background-destination-mobile.jpg",
+    desktop: "/assets/Destination/background-destination-desktop.jpg",
+    tablet: "/assets/Destination/background-destination-tablet.jpg",
+    mobile: "/assets/Destination/background-destination-mobile.jpg",
   },
   crew: {
-    desktop: "/assets/crew/background-crew-desktop.jpg",
-    tablet: "/assets/crew/background-crew-tablet.jpg",
-    mobile: "/assets/crew/background-crew-mobile.jpg",
+    desktop: "/assets/Crew/background-crew-desktop.jpg",
+    tablet: "/assets/Crew/background-crew-tablet.jpg",
+    mobile: "/assets/Crew/background-crew-mobile.jpg",
   },
   technology: {
-    desktop: "/assets/technology/background-technology-desktop.jpg",
-    tablet: "/assets/technology/background-technology-tablet.jpg",
-    mobile: "/assets/technology/background-technology-mobile.jpg",
+    desktop: "/assets/Technology/background-technology-desktop.jpg",
+    tablet: "/assets/Technology/background-technology-tablet.jpg",
+    mobile: "/assets/Technology/background-technology-mobile.jpg",
   },
 };
